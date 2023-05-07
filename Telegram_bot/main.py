5c6f751d50d13c490d0fec9febda9708652d2bbd
ThreeAi <alexandrovaa2003@gmail.com>
2023-05-08 02:59:41 +0300

    without debugging string

--- Telegram_bot/main.py
+++ Telegram_bot/main.py
@@ -24,16 +24,12 @@ def start(message):
     username = message.from_user.username
     url = f"{moodle_url}/webservice/rest/server.php?wstoken={moodle_token}&wsfunction=get_user_by_field_tg&username_tg={username}&moodlewsrestformat=json"
     response = requests.get(url)
-    print('1')
     id_moodle = json.loads(response.text)
-    print('2')
     if (id_moodle == None):
         bot.send_message(message.chat.id, 'вы не записали свой username')
-        print('in')
     else:
         id_moodle = json.loads(response.text)
         bot.send_message(message.chat.id, id_moodle)
-        print('in2')
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
