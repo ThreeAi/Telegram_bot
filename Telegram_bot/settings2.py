260317bf873498e33d9311a6c988abb7f9433f6c
ThreeAi <alexandrovaa2003@gmail.com>
2023-05-08 02:58:27 +0300

    add getcourses, start

---
+++ Telegram_bot/settings2.py
@@ -0,0 +1,3 @@
+bot_token = "6044058555:AAHPPGaxT8DJgo_uiT-v-LRmxRKCAXv89lg"
+moodle_token = "c1c2ddea47a1ad1c7c05c09b1cff2793"
+moodle_url = "https://moodle-monolith.spbstu.ru"
---
+++ Telegram_bot/settings3.py
@@ -0,0 +1,3 @@
+bot_token = "6044058555:AAHPPGaxT8DJgo_uiT-v-LRmxRKCAXv89lg"
+moodle_token = "c921fc8d0921cb0bfb49c821768d6065"
+moodle_url = "http://192.168.25.128/moodle"
